10ba83a3f09a465f1b5b80ad4ea405081464bc40
TaiWowAde <taiwo.adeyinka@prism.training>
2025-05-25 14:02:13 +0100

    initial commit

---
+++ basic_operations.py
@@ -0,0 +1,14 @@
+
+# Assigned values to variables
+number1 = 10
+number2 = 5
+
+# arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Print 
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
---
+++ hours_to_seconds.py
@@ -0,0 +1,7 @@
+
+hours = 2
+
+# Converts hours to seconds (1 hour = 3600 seconds)
+seconds = hours * 3600
+
+print(f"{hours} hour(s) is {seconds} seconds.")
