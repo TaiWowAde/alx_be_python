65bc7f31ff58565d979ca33eb439da92b76452b5
TaiWowAde <taiwo.adeyinka@prism.training>
2025-05-25 14:08:56 +0100

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,14 +1,6 @@
-
-# Assigned values to variables
 number1 = 10
 number2 = 5
 
-# arithmetic operations
-sum_result = number1 + number2
-difference_result = number1 - number2
-product_result = number1 * number2
-
-# Print 
-print(f"Addition of {number1} and {number2} is {sum_result}")
-print(f"Subtraction of {number1} and {number2} is {difference_result}")
-print(f"Multiplication of {number1} and {number2} is {product_result}")
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
