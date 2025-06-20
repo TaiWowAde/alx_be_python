077738c14ebc47711533a8c9cddafcb941668320
TaiWowAde <taiwow.adeyinka@gmail.com>
2025-06-20 10:34:58 +0100

    updated test divide

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,11 +20,9 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(0, 10), 0)
 
-    def test_division_normal(self):
+    def test_division_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
-
-    def test_division_by_zero(self):
         self.assertIsNone(self.calc.divide(10, 0))
 
 if __name__ == "__main__":
